3749c673117989a34b484f9b6c796a64e9c49b98
sly-designs <55442037+sly-designs@users.noreply.github.com>
2022-06-17 10:23:55 +0300

    My Portfolio
    A dark and light portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*typing animation*/
+var typed = new Typed(".typing",{
+	strings:["","Web Developer","Network Engineer","System developer","Content Writer","Mobile-app Developer"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
